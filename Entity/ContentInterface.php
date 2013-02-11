c14fdcd20537b7d7bbeed92e18e0b19459ce2d52
Matteo Caberlotto <mcaber@gmail.com>
2013-02-11 11:48:38 +0100

    updated ContentInterface

--- Entity/ContentInterface.php
+++ Entity/ContentInterface.php
@@ -3,17 +3,6 @@
 namespace Raindrop\RoutingBundle\Entity;
 
 interface ContentInterface {
-    
-    /**
-     * Returns the template definition for current action.
-     */
-    public function getTemplate();
-    
-    /**
-     * Returns the placeholder/value map for this template.
-     */
-    public function getArray();
-    
     /**
      * Returns last update of current document/entity.
      */
