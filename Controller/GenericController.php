8922391d37d7fa5435f2b121c2cf166d57212799
Matteo Caberlotto <mcaber@gmail.com>
2013-03-14 02:40:28 +0100

    minor refactor

--- Controller/GenericController.php
+++ Controller/GenericController.php
@@ -22,14 +22,21 @@ class GenericController extends Controller {
 
         $routeParams = $this->get('request')->query->all(); // do not lose eventual get parameters
 
+
         if ($content instanceof ExternalRouteInterface) {
+            /**
+             * External redirect
+             */
             $http_status = $content->getPermanent() ? 301 : 302;
             $uri = $content->getUri();
             if (count($routeParams)) {
                 $uri .= ((strpos($uri, '?') === false) ? '?' : '&') . http_build_query($routeParams);
             }
         } else {
-            $current_route = $this->get('raindrop_routing.route_repository')->findOneByName($this->getRequest()->get('_route'));
+            /**
+             * Inner redirect
+             */
+            $current_route = $this->get('router')->getRouteCollection()->get($this->getRequest()->get('_route'));
             $http_status = $current_route->getPermanent() ? 301 : 302;
             $uri = $this->get('router')->generate($content->getName(), $routeParams, true);
         }
